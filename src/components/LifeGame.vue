01320fd87b5fbd0762725911c42a3de9c92d56df
Fernanda Liz <fernandaliz2004@hotmail.com>
2022-12-28 10:01:20 -0300

    corrige matriz

--- src/components/LifeGame.vue
+++ src/components/LifeGame.vue
@@ -9,42 +9,6 @@
     </div>
 </template>
 <script>
-function _count_neighboors_alive (i, j, grid) {
-    const [M, N] = [grid.length, grid[0].length]
-    const delta_coord = [
-        [-1, -1], [-1, 0], [-1, 1],
-        [0, -1], [0, 1],
-        [1, -1], [1, 0], [1, 1],
-    ]
-    let coord_neighboors = []
-    for (let k = 0; k < delta_coord.length; k++) {
-        coord_neighboors.push([i + delta_coord[k][0], j + delta_coord[k][1]])
-    }
-    let coord_neighboors_alive = coord_neighboors.filter((e) => e[0] >= 0 && e[0] < M && e[1] >= 0 && e[1] < N && grid[e[0]][e[1]] == 1)
-    return coord_neighboors_alive.length
-}
-function _nextState(grid) {
-    const [M, N] = [grid.length, grid[0].length]
-    let newGrid = new Array(M)
-    for (let i = 0; i < M; i++) {
-        for (let j = 0; j < N; j++) {
-            let neighboorsAlive = _count_neighboors_alive(i, j, grid)
-            if (grid[i][j] == 1) {
-                if (neighboorsAlive == 2 || neighboorsAlive == 3) {
-                    newGrid[i][j] = 1
-                } else {
-                    newGrid[i][j] = 0
-                }
-            } else {
-                if (neighboorsAlive == 3) {
-                    newGrid[i][j] = 1
-                }
-            }
-        }
-    }
-    return newGrid
-}
-
 export default {
 data() {
     return {
@@ -64,8 +28,45 @@ data() {
 },
 methods: {
     turnAlive() {
-        this.grid = _nextState(this.grid)
+        this.grid = this._nextState(this.grid)
     },
+    _count_neighboors_alive(i, j, grid) {
+    const [M, N] = [grid.length, grid[0].length]
+    const delta_coord = [
+        [-1, -1], [-1, 0], [-1, 1],
+        [0, -1], [0, 1],
+        [1, -1], [1, 0], [1, 1],
+    ]
+    let coord_neighboors = []
+    for (let k = 0; k < delta_coord.length; k++) {
+        coord_neighboors.push([i + delta_coord[k][0], j + delta_coord[k][1]])
+    }
+    let coord_neighboors_alive = coord_neighboors.filter((e) => e[0] >= 0 && e[0] < M && e[1] >= 0 && e[1] < N && grid[e[0]][e[1]] == 1)
+    return coord_neighboors_alive.length
+    },
+    _nextState(grid) {
+    const [M, N] = [grid.length, grid[0].length]
+    let newGrid = JSON.parse(JSON.stringify(this.grid))
+    for (let i = 0; i < M; i++) {
+        for (let j = 0; j < N; j++) {
+            let neighboorsAlive = this._count_neighboors_alive(i, j, grid)
+            if (grid[i][j] == 1) {
+                if (neighboorsAlive == 2 || neighboorsAlive == 3) {
+                    newGrid[i][j] = 1
+                } else {
+                    newGrid[i][j] = 0
+                }
+            } else {
+                if (neighboorsAlive == 3) {
+                    newGrid[i][j] = 1
+                } else {
+                    newGrid[i][j] = 0
+                }
+            }
+        }
+    }
+    return newGrid
+},
 },
 mounted() {
     setInterval(() => {
