b9890516b23a8eafb5e16efe136fe2eecdf12d37
Fernanda Liz <fernandaliz2004@hotmail.com>
2022-12-29 11:45:43 -0300

    aumenta grid e ajusta css

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #3d3da5;
   margin-top: 40px;
 }
 </style>
